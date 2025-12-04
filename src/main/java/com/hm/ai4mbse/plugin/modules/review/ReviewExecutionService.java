a8005705fe6c285db9ccdec19489e647ef952fb8
dbr-SE <d.br.private@gmail.com>
2025-12-04 23:20:34 +0100

    Add valid placeholders for remaining modules (rules, review)

---
+++ src/main/java/com/hm/ai4mbse/plugin/modules/review/ReviewExecutionService.java
@@ -0,0 +1,5 @@
+package com.hm.ai4mbse.plugin.modules.review;
+
+public class ReviewExecutionService {
+    // Hier kommt spaeter die Logik rein, die das Modell gegen die Regeln prüft
+}
---
+++ src/main/java/com/hm/ai4mbse/plugin/modules/rules/RuleGenerator.java
@@ -0,0 +1,5 @@
+package com.hm.ai4mbse.plugin.modules.rules;
+
+public class RuleGenerator {
+    // Hier kommt spaeter Michis Logik rein
+}
